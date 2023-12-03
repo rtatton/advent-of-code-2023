a4be3f256c4954679ef6b049e9d3b2348c8d9c83
Ryan <rttatton@amazon.com>
2023-12-02 23:24:42 -0500

    refactor!: day 1 - add support for words and digits

--- lib/src/main/java/code/Day1.java
+++ lib/src/main/java/code/Day1.java
@@ -1,39 +1,72 @@
 package code;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
-public class Day1 {
+public record Day1(Pattern pattern, int groupIndex) {
+
+  private static final Map<String, String> WORD_TO_DIGIT = wordToDigit();
+
+  public static Day1 digits() {
+    return new Day1(Pattern.compile("\\d"), 0);
+  }
+
+  public static Day1 digitsAndWords() {
+    var regex = "(?=(" + String.join("|", WORD_TO_DIGIT.keySet()) + "|\\d))";
+    var pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+    return new Day1(pattern, 1);
+  }
 
   public static void main(String[] args) {
-    try (var reader = Files.newBufferedReader(Path.of("lib/src/main/resources/day-1.txt"))) {
-      System.out.println(reader.lines().mapToLong(Day1::valueOf).sum());
+    var input = Path.of("lib/src/main/resources/day-1.txt");
+    System.out.println("Digits: " + Day1.digits().compute(input));
+    System.out.println("Digits and words: " + Day1.digitsAndWords().compute(input));
+  }
+
+  public long compute(Path input) {
+    try (var reader = Files.newBufferedReader(input)) {
+      return reader.lines().mapToLong(this::valueOf).sum();
     } catch (IOException e) {
-      throw new RuntimeException(e);
+      throw new UncheckedIOException(e);
     }
   }
 
-  private static long valueOf(String line) {
+  private int valueOf(String line) {
     var digits = digits(line);
-    if (digits.isEmpty()) {
-      return 0;
-    } else if (digits.size() == 1) {
-      return Long.parseLong(digits.get(0) + digits.get(0));
-    } else {
-      return Long.parseLong(digits.get(0) + digits.get(digits.size() - 1));
-    }
+    return digits.isEmpty() ? 0 : digits.get(0) * 10 + digits.get(digits.size() - 1);
   }
 
-  private static List<String> digits(String line) {
-    var digits = new ArrayList<String>();
-    for (char c : line.toCharArray()) {
-      if (Character.isDigit(c)) {
-        digits.add(String.valueOf(c));
-      }
+  private List<Integer> digits(String line) {
+    var matcher = pattern.matcher(line);
+    var digits = new ArrayList<Integer>();
+    while (matcher.find()) {
+      var group = matcher.group(groupIndex);
+      digits.add(toInt(group));
     }
     return digits;
+  }
+
+  private int toInt(String wordOrDigit) {
+    return Integer.parseInt(WORD_TO_DIGIT.getOrDefault(wordOrDigit.toLowerCase(), wordOrDigit));
+  }
+
+  private static Map<String, String> wordToDigit() {
+    return Map.ofEntries(
+        Map.entry("zero", "0"),
+        Map.entry("one", "1"),
+        Map.entry("two", "2"),
+        Map.entry("three", "3"),
+        Map.entry("four", "4"),
+        Map.entry("five", "5"),
+        Map.entry("six", "6"),
+        Map.entry("seven", "7"),
+        Map.entry("eight", "8"),
+        Map.entry("nine", "9"));
   }
 }
